d6ef2095305464238c122f3eece7eecdb186ab99
Ryan Altvater <ryan@ryanaltvater.com>
2016-05-25 14:18:31 -0400

    added new generator tasks

--- generators/static/index.js
+++ generators/static/index.js
@@ -1,48 +1,103 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
+var yeoman = require('yeoman-generator'),
+    chalk = require('chalk'),
+    yosay = require('yosay'),
+    shell = require('shelljs'),
+    liquidWordPressGenerator = yeoman.Base.extend({
+        prompting: function () {
+            var done = this.async();
 
-  prompting: function () {
-    var done = this.async();
+            // Prompts the user for some project information
+            var prompts = [
+                {
+                    message: 'Project name',
+                    name: 'projectName',
+                    type: 'input',
+                    default: 'New Project'
+                }
+            ];
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the splendid ' + chalk.red('Liquid') + ' generator!'
-    ));
+            this.prompt(prompts, function (props) {
+                this.projectName = props.projectName;
+                this.startVagrant = props.startVagrant;
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+                done();
+            }.bind(this));
+        },
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someAnswer;
+        writing: function () {
+            this.log(chalk.magenta('Setting up project files...'));
 
-      done();
-    }.bind(this));
-  },
+            // Copies "dotfiles" to the project folder
+            this.fs.copy(
+                this.templatePath('../../app/templates/dotfiles/.*'),
+                this.destinationPath('./')
+            );
 
-  writing: function () {
-    // Scotch Box (Vagrant)
-    this.fs.copy(
-      this.templatePath('Vagrantfile'),
-      this.destinationPath('Vagrantfile')
-    );
+            // Copies Scotch Box's "Vagrantfile" to the project folder
+            this.fs.copy(
+                this.templatePath('../../app/templates/vagrant/Vagrantfile'),
+                this.destinationPath('Vagrantfile')
+            );
 
-    // Scotch Box Config
-    this.fs.copy(
-      this.templatePath('config.php'),
-      this.destinationPath('public/config.php')
-    );
-  },
+            // Copies Scotch Box's Vagrant "config.php" to the theme folder
+            this.fs.copy(
+                this.templatePath('../../app/templates/vagrant/config.php'),
+                this.destinationPath('public/config.php')
+            );
 
-  // install: function () {
-  //   this.installDependencies();
-  // }
-});
+            // Copies "dependencies" to the "src" theme folder
+            this.fs.copyTpl(
+              this.templatePath('../../app/templates/dependencies/*'),
+              this.destinationPath('./'),
+              {
+                projectType: 'static',
+                projectName: this.projectName.replace(/ /g, '-'),
+                projectNameLower: this.projectName.toLowerCase().replace(/ /g, '-'),
+              }
+            );
+
+            // Copies "gulpfile.js" to the project folder
+            this.fs.copy(
+                this.templatePath('gulp/gulpfile.js'),
+                this.destinationPath('gulpfile.js')
+            );
+
+            // Copies "liquid" theme to the "src" folder
+            this.fs.copy(
+                this.templatePath('src/**'),
+                this.destinationPath('src/')
+            );
+
+            // Copies "favicons" to the "src" theme folder
+            this.fs.copy(
+                this.templatePath('../../app/templates/favicons/*'),
+                this.destinationPath('src/')
+            );
+
+            // Copies "assets" to the "src" theme folder
+            this.fs.copy(
+                this.templatePath('../../app/templates/assets/**'),
+                this.destinationPath('src/assets/')
+            );
+        },
+
+        install: function () {
+            var done = this.async();
+
+            this.log(chalk.magenta('Starting Vagrant...'));
+
+            // Starts Vagrant server
+            shell.exec('vagrant up');
+
+            done();
+
+            this.log(chalk.magenta('Installing dependencies...'));
+
+            // Installs Node and Bower dependencies
+            this.installDependencies();
+        }
+    });
+
+module.exports = liquidWordPressGenerator;
